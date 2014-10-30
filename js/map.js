2729069f99154fc6a4d612413ba1f52f3c02f5ec
Torsten Compart <torsten.compart@gmail.com>
2014-10-30 23:12:03 +0100

    fixing not working google map script

--- js/map.js
+++ js/map.js
@@ -1,22 +1,23 @@
 /*globals google*/
-function initialize() {
-  var mapCanvas = document.getElementById('map_canvas');
-  var latLong = new google.maps.LatLng(52.632640, 13.495620);
-  var mapOptions = {
-    center: latLong,
-    zoom: 16,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-  var map = new google.maps.Map(mapCanvas, mapOptions);
+! function () {
+  function initialize() {
+    var mapCanvas = document.getElementById('map_canvas');
+    var latLong = new google.maps.LatLng(52.632640, 13.495620);
+    var mapOptions = {
+      center: latLong,
+      zoom: 16,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(mapCanvas, mapOptions);
 
-  var marker = new google.maps.Marker({
-    position: latLong,
-    map: map,
-    title: 'Panke & Meer'
-  });
-}
+    new google.maps.Marker({
+      position: latLong,
+      map: map,
+      title: 'Panke & Meer'
+    });
+  }
 
-var google = google || null;
-if (google !== null) {
-  google.maps.event.addDomListener(window, 'load', initialize);
-}
+  if (typeof google !== 'undefined') {
+    google.maps.event.addDomListener(window, 'load', initialize);
+  }
+}();
