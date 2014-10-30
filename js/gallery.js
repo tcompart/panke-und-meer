a5fcac54d1cb5b50b02d8292178b3b3069b996f0
Torsten Compart <torsten.compart@gmail.com>
2014-10-30 23:10:32 +0100

    making image gallery more resilient fallback implement with little thumbnails

--- js/gallery.js
+++ js/gallery.js
@@ -1,12 +1,24 @@
 /*global Galleria*/
-if (Galleria) {
-  Galleria.loadTheme('js/vendor/galleria-classic-theme/galleria.classic.min.js');
-  Galleria.configure({
-    imageCrop: false,
-    transition: 'fade',
-    responsive: true,
-    showCounter: false,
-    swipe: true
+;!(function($) {
+  $(document).ready(function() {
+    if (typeof Galleria === 'undefined') {
+      var galleryElement = $('#gallery');
+      galleryElement.removeClass('galleria');
+      galleryElement.children().each(function () {
+        var element = $(this);
+        console.log("element: ", element);
+        element.addClass('thumbnails');
+      });
+    } else {
+      Galleria.loadTheme('js/vendor/galleria-classic-theme/galleria.classic.min.js');
+      Galleria.configure({
+        imageCrop: false,
+        transition: 'fade',
+        responsive: true,
+        showCounter: false,
+        swipe: true
+      });
+      Galleria.run('.galleria');
+    }
   });
-  Galleria.run('.galleria');
-}
+})(jQuery);
